895ba6a31a0b6d4462e7223857d421e00f6a26e7
Brenindepay <brenorv1518@icloud.com>
2026-04-27 11:18:28 -0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,21 @@
+python
+
+import sqlite3
+
+def conectar ():
+  return sqlite3.connect("estoque.db")
+
+def criar_tabela():
+  conn = conectar()
+  cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS produtos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    preco REAL NOT NULL
+)
+""")
+
+conn.comit()
+conn.close()
